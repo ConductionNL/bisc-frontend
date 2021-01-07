29e054d3be5a7fdf15f4d381ea530251cfa3871f
Dirk Kok <dirk@lifely.nl>
2021-01-07 16:44:53 +0100

    feat(auth): disable unused JwtStrategy

--- server/src/User/strategies/JwtStrategy.ts
+++ server/src/User/strategies/JwtStrategy.ts
@@ -1,19 +1,21 @@
-import { ExtractJwt, Strategy } from 'passport-jwt'
-import { PassportStrategy } from '@nestjs/passport'
-import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+// TODO: We need this when we want to use AuthGuard('jwt') in JwtAuthGuard, keeping it commented for now
 
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    public constructor(configService: ConfigService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: configService.get('APP_SECRET'),
-        })
-    }
+// import { ExtractJwt, Strategy } from 'passport-jwt'
+// import { PassportStrategy } from '@nestjs/passport'
+// import { Injectable } from '@nestjs/common'
+// import { ConfigService } from '@nestjs/config'
 
-    public async validate(payload: { sub: number; username: string }) {
-        return { userId: payload.sub, username: payload.username }
-    }
-}
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//     public constructor(configService: ConfigService) {
+//         super({
+//             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//             ignoreExpiration: false,
+//             secretOrKey: configService.get('APP_SECRET'),
+//         })
+//     }
+
+//     public async validate(payload: { sub: number; username: string }) {
+//         return { userId: payload.sub, username: payload.username }
+//     }
+// }
