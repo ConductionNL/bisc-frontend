ce8f857c920ae961cdb22f0ef4ce3ae3628b03f2
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-11 09:33:03 +0100

    fix plural

--- client/src/components/Core/Breadcrumb/Breadcrumbs.tsx
+++ client/src/components/Core/Breadcrumb/Breadcrumbs.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import classNames from 'classnames'
 
-import styles from './Breadcrumb.module.scss'
+import styles from './Breadcrumbs.module.scss'
 
 interface Props {
     className?: string
 }
 
-const Breadcrumb: React.FunctionComponent<Props> = props => {
+const Breadcrumbs: React.FunctionComponent<Props> = props => {
     const { className, children } = props
     const containerClassNames = classNames(styles.container, className)
 
     return <div className={containerClassNames}>{children}</div>
 }
 
-export default Breadcrumb
+export default Breadcrumbs
