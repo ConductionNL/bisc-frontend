cce58444cf2bd340f3f95a3851cae8b2b0f0fdee
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-11 14:37:38 +0100

    add spacing

--- client/src/views/Unauthorized/LoginView.tsx
+++ client/src/views/Unauthorized/LoginView.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Button from '../../components/Core/Button/Button'
 import Input from '../../components/Core/DataEntry/Input'
+import Column from '../../components/Core/Layout/Column/Column'
 import ContentGreetingPageLayout from '../../components/Core/PageLayout/ContentGreetingPageLayout'
 import PageTitle from '../../components/Core/Text/PageTitle'
 import Paragraph from '../../components/Core/Typography/Paragraph'
@@ -9,13 +11,20 @@ function LoginView() {
         <ContentGreetingPageLayout
             greeting={'Welkom bij Top'}
             ContentComponent={
-                <>
-                    <PageTitle title={'Log in'} />
-                    <Paragraph>Welkom terug! Log in met je email en wachtwoord.</Paragraph>
+                <Column spacing={8}>
+                    <Column>
+                        <PageTitle title={'Log in'} />
+                        <Paragraph>Welkom terug! Log in met je email en wachtwoord.</Paragraph>
+                    </Column>
                     <hr />
-                    <Input placeholder={'john@doe.com'} />
-                    <Input placeholder={'6+ Karakters'} />
-                </>
+                    <Column spacing={12}>
+                        <Column spacing={6}>
+                            <Input placeholder={'john@doe.com'} />
+                            <Input placeholder={'6+ Karakters'} />
+                        </Column>
+                        <Button stretch={true}>Inloggen</Button>
+                    </Column>
+                </Column>
             }
         />
     )
