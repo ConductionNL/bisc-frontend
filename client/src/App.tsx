e151ebc765b0ccfa7ccaf779f5db1eef892b3f6d
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-11 14:16:47 +0100

    fix app style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { routes } from './routes'
+import styles from './App.module.scss'
 import AddPersonToProgramView from './views/AddPersonToProgramView'
 import AddPersonView from './views/AddPersonView'
 import IndexView from './views/IndexView'
@@ -12,18 +13,20 @@ import LoginView from './views/Unauthorized/LoginView'
 
 function App() {
     return (
-        <Switch>
-            <Route path={routes.index} exact={true} component={IndexView} />
-            <Route path={routes.login} exact={true} component={LoginView} />
-            <Route path={routes.persons} exact={true} component={PersonsView} />
-            <Route path={routes.addPerson} exact={true} component={AddPersonView} />
-            <Route path={routes.addPersonToProgram} exact={true} component={AddPersonToProgramView} />
-            <Route path={routes.programs} exact={true} component={ProgramsView} />
-            <Route path={routes.myPrograms} exact={true} component={MyProgramsView} />
+        <div className={styles.container}>
+            <Switch>
+                <Route path={routes.index} exact={true} component={IndexView} />
+                <Route path={routes.login} exact={true} component={LoginView} />
+                <Route path={routes.persons} exact={true} component={PersonsView} />
+                <Route path={routes.addPerson} exact={true} component={AddPersonView} />
+                <Route path={routes.addPersonToProgram} exact={true} component={AddPersonToProgramView} />
+                <Route path={routes.programs} exact={true} component={ProgramsView} />
+                <Route path={routes.myPrograms} exact={true} component={MyProgramsView} />
 
-            {/* TODO: delete - for design review/check only */}
-            <Route path={routes.kitchensink} exact={true} component={Kitchensink} />
-        </Switch>
+                {/* TODO: delete - for design review/check only */}
+                <Route path={routes.kitchensink} exact={true} component={Kitchensink} />
+            </Switch>
+        </div>
     )
 }
 
