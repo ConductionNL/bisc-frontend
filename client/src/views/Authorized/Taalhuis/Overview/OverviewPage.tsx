df9996b3f6f002a54ec876f580f1e7829976a4ce
bbawuah <brianbawuah@icloud.com>
2021-02-19 15:34:29 +0100

    feat(inputs): add correct inputs

--- client/src/views/Authorized/Taalhuis/Overview/OverviewPage.tsx
+++ client/src/views/Authorized/Taalhuis/Overview/OverviewPage.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Actionbar from '../../../../components/Core/Actionbar/Actionbar'
+import Breadcrumb from '../../../../components/Core/Breadcrumb/Breadcrumb'
+import Breadcrumbs from '../../../../components/Core/Breadcrumb/Breadcrumbs'
 import Button, { ButtonType } from '../../../../components/Core/Button/Button'
 import Input from '../../../../components/Core/DataEntry/Input'
-import Password from '../../../../components/Core/DataEntry/Password'
 import { NotificationsManager } from '../../../../components/Core/Feedback/Notifications/NotificationsManager'
-import PasswordStrengthBar from '../../../../components/Core/Feedback/PasswordStrengthBar/PasswordStrengthBar'
 import Field from '../../../../components/Core/Field/Field'
 import Section from '../../../../components/Core/Field/Section'
+import HorizontalRule from '../../../../components/Core/HorizontalRule/HorizontalRule'
 import Column from '../../../../components/Core/Layout/Column/Column'
 import Space from '../../../../components/Core/Layout/Space/Space'
 import PageTitle, { PageTitleSize } from '../../../../components/Core/Text/PageTitle'
+import { routes } from '../../../../routes'
 
 interface Props {}
 
@@ -17,23 +19,42 @@ const OverviewPage: React.FunctionComponent<Props> = () => {
     return (
         <>
             <Column spacing={12}>
-                <PageTitle title={'Nieuwe medewerker'} size={PageTitleSize.default} />
-                <Section title={'Wachtwoord aanpassen'}>
+                <Breadcrumbs>
+                    <Breadcrumb text={'test 1'} to={routes.authorized.kitchensink} />
+                    <Breadcrumb text={'test 1'} />
+                    <Breadcrumb text={'test 1'} />
+                    <Breadcrumb text={'test 1'} />
+                </Breadcrumbs>
+                <PageTitle title={'Nieuwe taalhuis'} size={PageTitleSize.default} />
+                <Section title={'Vestiging'}>
                     <Column spacing={4}>
-                        <Field label={'Gegevens'} horizontal={true} required={true}>
-                            <Input required={true} name="achternaam" placeholder={'Achternaam'} onChange={undefined} />
+                        <Field label={'Naam Taalhuis'} horizontal={true} required={true}>
+                            <Input required={true} name="taalhuis" placeholder={'Taalhuis X'} onChange={undefined} />
                         </Field>
 
-                        <Field label={'Tussenvoegsel'} horizontal={true}>
-                            <Input name="tussenvoegsel" placeholder={'Tussenvoegsel'} onChange={undefined} />
+                        <Field label={'Straat en huisnr.'} horizontal={true}>
+                            <Input name="straatnaam" placeholder={'Straatnaam'} onChange={undefined} />
                         </Field>
 
-                        <Field label={'Roepnaam'} horizontal={true} required={true}>
-                            <Input required={true} name="roepnaam" placeholder={'Roepnaam'} onChange={undefined} />
+                        <Field label={'Postcode'} horizontal={true}>
+                            <Input name="postcode" placeholder={'1234AB'} onChange={undefined} />
                         </Field>
 
-                        <Field label={'Telefoonnumber'} horizontal={true}>
-                            <Input name="telefoonnummer" placeholder={'Telefoonnummer'} onChange={undefined} />
+                        <Field label={'Plaats'} horizontal={true}>
+                            <Input name="plaatsnaam" placeholder={'Utrecht'} onChange={undefined} />
+                        </Field>
+                    </Column>
+                </Section>
+            </Column>
+            <HorizontalRule />
+            <Column spacing={12}>
+                <Section title={'Contactgegevens'}>
+                    <Column spacing={4}>
+                        <Field label={'Telefoonnummer'} horizontal={true}>
+                            <Input name="telefoonnummer" placeholder={'030 - 123 45 67'} onChange={undefined} />
+                        </Field>
+                        <Field label={'E-mailadres'} horizontal={true}>
+                            <Input name="email" placeholder={'Taalhuis@email.nl'} onChange={undefined} />
                         </Field>
                     </Column>
                 </Section>
@@ -41,9 +62,18 @@ const OverviewPage: React.FunctionComponent<Props> = () => {
             <Space pushTop={true} />
             <Actionbar
                 RightComponent={
-                    <Button type={ButtonType.primary} onClick={() => NotificationsManager.success('title', 'test')}>
-                        Wachtwoord wijzigen
-                    </Button>
+                    <Column>
+                        <Button
+                            type={ButtonType.secondary}
+                            onClick={() => NotificationsManager.success('title', 'test')}
+                        >
+                            Annuleren
+                        </Button>
+
+                        <Button type={ButtonType.primary} onClick={() => NotificationsManager.success('title', 'test')}>
+                            Toevoegen
+                        </Button>
+                    </Column>
                 }
             />
         </>
