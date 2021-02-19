b40f70457aeef22c79bec71a9f16b15d2f6afe65
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-19 16:08:02 +0100

    add react-modal package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import RootView from './views/RootView'
 import { NotificationsManager } from './components/Core/Feedback/Notifications/NotificationsManager'
+import { default as ReactModal } from 'react-modal'
+ReactModal.setAppElement('#root')
+
 function App() {
     return (
         <>
