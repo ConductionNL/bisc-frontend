6f9f44b6f0d3a5eca90f077ade6f8ca1dcfa7f9a
bbawuah <brianbawuah@icloud.com>
2021-02-23 16:26:19 +0100

    feat(medewerkers): add table to medewerkers page

--- client/src/views/Authorized/Taalhuis/Overview/TaalhuizenOverviewReadView/MedewerkersView.tsx
+++ client/src/views/Authorized/Taalhuis/Overview/TaalhuizenOverviewReadView/MedewerkersView.tsx
@@ -1,6 +1,7 @@
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import React from 'react'
+import { Table } from '../../../../../components/Core/Table/Table'
 import Paragraph from '../../../../../components/Core/Typography/Paragraph'
 
 interface Props {}
@@ -10,7 +11,15 @@ const Medewerkers: React.FunctionComponent<Props> = () => {
 
     return (
         <>
-            <Paragraph>{i18n._(t`Medewerkers`)}</Paragraph>
+            <Table
+                flex={1}
+                headers={[i18n._(t`NAAM`), i18n._(t`ADRES`), i18n._(t`PLAATS`)]}
+                rows={[
+                    [<p>Test</p>, <p>Test</p>, <p>Test</p>],
+                    [<p>Test</p>, <p>Test</p>, <p>Test</p>],
+                    [<p>Test</p>, <p>Test</p>, <p>Test</p>],
+                ]}
+            />
         </>
     )
 }
