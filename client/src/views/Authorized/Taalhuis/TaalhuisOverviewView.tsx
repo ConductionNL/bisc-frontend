f825f0929d1f7d98d6c4f684e3f047ed502acaa2
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-23 10:58:16 +0100

    remove test

--- client/src/views/Authorized/Taalhuis/TaalhuisOverviewView.tsx
+++ client/src/views/Authorized/Taalhuis/TaalhuisOverviewView.tsx
@@ -4,10 +4,5 @@ import PageTitle from '../../../components/Core/Text/PageTitle'
 interface Props {}
 
 export const TaalhuisOverviewView: React.FunctionComponent<Props> = () => {
-    return (
-        <>
-            test
-            <PageTitle title={'Taalhuis overview'} />
-        </>
-    )
+    return <PageTitle title={'Taalhuis overview'} />
 }
