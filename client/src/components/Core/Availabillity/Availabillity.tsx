5e5e0a0b167cc9c79f3c9c24c0d3235e0a01f8db
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-24 17:35:36 +0100

    add availabillity styling

--- client/src/components/Core/Availabillity/Availabillity.tsx
+++ client/src/components/Core/Availabillity/Availabillity.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import classNames from 'classnames'
-
-import styles from './Availabillity.module.scss'
 import { i18n } from '@lingui/core'
 import { t } from '@lingui/macro'
-import Checkbox from '../DataEntry/Checkbox'
+import classNames from 'classnames'
 import times from 'lodash/times'
+import React from 'react'
+import Checkbox from '../DataEntry/Checkbox'
+import styles from './Availabillity.module.scss'
 
 interface Props {
     className?: string
@@ -28,32 +27,34 @@ const Availabillity: React.FunctionComponent<Props> = props => {
         <table className={containerClassNames}>
             <thead className={styles.thead}>
                 <tr>
-                    <th />
+                    <th className={styles.headerBlank} />
 
                     {days.map(day => (
-                        <th>{day}</th>
+                        <th>
+                            <p className={styles.headerName}>{day}</p>
+                        </th>
                     ))}
                 </tr>
             </thead>
-            <tbody>
+            <tbody className={styles.tbody}>
                 <tr>
-                    <td className={styles.td}>
-                        <p>{i18n._(t`Ochtend`)}</p>
-                        <p>{i18n._(t`06:00 - 12:00`)}</p>
+                    <td className={styles.sectionOfDayWrapper}>
+                        <p className={styles.sectionOfDay}>{i18n._(t`Ochtend`)}</p>
+                        <p className={styles.timeOfDay}>{i18n._(t`06:00 - 12:00`)}</p>
                     </td>
                     {renderCheckboxes('morning')}
                 </tr>
                 <tr>
-                    <td className={styles.td}>
-                        <p>{i18n._(t`Middag`)}</p>
-                        <p>{i18n._(t`12:00 - 18:00`)}</p>
+                    <td className={styles.sectionOfDayWrapper}>
+                        <p className={styles.sectionOfDay}>{i18n._(t`Middag`)}</p>
+                        <p className={styles.timeOfDay}>{i18n._(t`12:00 - 18:00`)}</p>
                     </td>
                     {renderCheckboxes('midday')}
                 </tr>
                 <tr>
-                    <td className={styles.td}>
-                        <p>{i18n._(t`Avond`)}</p>
-                        <p>{i18n._(t`18:00 - 00:00`)}</p>
+                    <td className={styles.sectionOfDayWrapper}>
+                        <p className={styles.sectionOfDay}>{i18n._(t`Avond`)}</p>
+                        <p className={styles.timeOfDay}>{i18n._(t`18:00 - 00:00`)}</p>
                     </td>
                     {renderCheckboxes('evening')}
                 </tr>
@@ -64,7 +65,7 @@ const Availabillity: React.FunctionComponent<Props> = props => {
     function renderCheckboxes(timeOfDay: string) {
         return times(7, n => (
             <td key={n} className={styles.checkBoxTd}>
-                <Checkbox name={`${timeOfDay}-${days[n]}`} />
+                <Checkbox name="availabillity" value={`${timeOfDay}-${days[n]}`} id={`${timeOfDay}-${days[n]}`} />
             </td>
         ))
     }
