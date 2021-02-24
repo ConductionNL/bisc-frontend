bb3fadef3a0ccefe2c306eda31e243a37b6b047d
Dirk Kok <dirk@lifely.nl>
2021-02-24 16:50:18 +0100

    feat(taalhuis): WIP code for taalhuizen query (cant test or generate graphql code because cc api is down)

--- server/src/Taalhuis/TaalhuisRepository.ts
+++ server/src/Taalhuis/TaalhuisRepository.ts
@@ -14,4 +14,24 @@ export class TaalhuisRepository extends CCRepository {
         //
         return !!input
     }
+
+    public async findAll(): Promise<
+        { id: string; name: string; addresses: { id: string; houseNumber: string; postalCode: string }[] }[]
+    > {
+        const result = await this.sdk.taalhuizen()
+
+        const organisations = result?.organisations?.edges
+
+        // TODO: Fetch addresses through a dataloader?
+        const organisationsWithAddressMapped = organisations.map(organisation => {
+            const addresses = organisation.edges.map(addressEdge => {
+                return this.returnNonNullable(addressEdge.node)
+            })
+            organisation.addresses = addresses
+
+            return this.returnNonNullable(organisation)
+        })
+
+        return organisationsWithAddressMapped
+    }
 }
--- server/src/Taalhuis/TaalhuisResolver.ts
+++ server/src/Taalhuis/TaalhuisResolver.ts
@@ -1,11 +1,23 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { CreateTaalhuisInputType } from './types/CreateTaalhuisInputType'
 import { CreateTaalhuisService } from './CreateTaalhuisService'
 import { TaalhuisType } from './types/TaalhuisType'
+import { CurrentUser } from 'src/User/CurrentUserDecorator'
+import { UserEntity } from 'src/User/entities/UserEntity'
+import { TaalhuisRepository } from './TaalhuisRepository'
 
 @Resolver()
 export class TaalhuisResolver {
-    public constructor(private createTaalhuisService: CreateTaalhuisService) {}
+    public constructor(
+        private createTaalhuisService: CreateTaalhuisService,
+        private taalhuisRepository: TaalhuisRepository
+    ) {}
+
+    @Query(() => [TaalhuisType])
+    public async taalhuizen(@CurrentUser() user: UserEntity): Promise<TaalhuisType[]> {
+        // TODO: Authorization checks (user type, user role)
+        return this.taalhuisRepository.findAll()
+    }
 
     @Mutation(() => TaalhuisType)
     public async createTaalhuis(@Args() args: CreateTaalhuisInputType): Promise<TaalhuisType> {
