77aa362e36e8bd1bee1d767e4a6fba3c853bc112
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-24 16:03:39 +0100

    add insertion validators

---
+++ client/src/utils/validators/InsertionValidator.ts
@@ -0,0 +1,18 @@
+import { i18n } from '@lingui/core'
+import { t } from '@lingui/macro'
+import { GenericValidators } from './GenericValidators'
+interface StringsMatch {
+    newPassword?: string
+    repeatPassword?: string
+}
+class Insertion {
+    public isValidInsertion = (value: string) => {
+        const check = [GenericValidators.noCapitals(value), GenericValidators.noSpecialCharacters(value)]
+        if (check.every(validator => validator !== null)) {
+            return check[0]
+        }
+        return null
+    }
+}
+
+export const InsertionValidators = new Insertion()
