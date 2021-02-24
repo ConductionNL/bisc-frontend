05557c90c592a11be3edd6ed459a9e003f51297f
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-24 20:04:39 +0100

    remove unused stuff

--- client/src/utils/validators/InsertionValidator.ts
+++ client/src/utils/validators/InsertionValidator.ts
@@ -1,10 +1,4 @@
-import { i18n } from '@lingui/core'
-import { t } from '@lingui/macro'
 import { GenericValidators } from './GenericValidators'
-interface StringsMatch {
-    newPassword?: string
-    repeatPassword?: string
-}
 class Insertion {
     public isValidInsertion = (value: string | null) => {
         const check = [GenericValidators.noCapitals(value), GenericValidators.noSpecialCharacters(value)]
