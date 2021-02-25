b84cfb8e04fcbc2c3ee2493431918a104ad64a04
Rick Woltheus <rick.woltheus@lifely.nl>
2021-02-25 16:07:12 +0100

    add prefill and readonly

--- client/src/components/fieldsets/shared/AccountInformationFieldset.tsx
+++ client/src/components/fieldsets/shared/AccountInformationFieldset.tsx
@@ -2,6 +2,7 @@
 
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
+import isEqual from 'lodash/isEqual'
 import React from 'react'
 import LabelTag, { LabelColor } from '../../Core/DataDisplay/LabelTag/LabelTag'
 import Input from '../../Core/DataEntry/Input'
@@ -11,37 +12,74 @@ import Section from '../../Core/Field/Section'
 import Column from '../../Core/Layout/Column/Column'
 import Row from '../../Core/Layout/Row/Row'
 
-interface Props {}
+interface Props {
+    prefillData?: AccountInformationFieldsetModal
+    readOnly?: boolean
+}
 
-const AccountInformationFieldset: React.FunctionComponent<Props> = () => {
+interface AccountInformationFieldsetModal {
+    email: string
+    roles: Roles[]
+} 
+
+export enum Roles {
+    coordinator = 'coordinator',
+    mentor = 'mentor',
+    volunteer = 'volunteer' 
+}
+
+const AccountInformationFieldset: React.FunctionComponent<Props> = props => {
+    const {prefillData, readOnly} = props
     const { i18n } = useLingui()
+    const colorConfig = {
+        [Roles.coordinator]: LabelColor.red,
+        [Roles.mentor]: LabelColor.purple,
+        [Roles.volunteer]: LabelColor.yellow,
+    }
+
+    if (readOnly) {
+        return (
+            <Section title={i18n._(t`Accountgegevens`)}>
+                <Column spacing={6}>
+                    <Field label={i18n._(t`Email`)} horizontal={true}>
+                        <p>{prefillData?.email}</p>
+                    </Field>
+                    <Field label={i18n._(t`Rol`)} horizontal={true}>
+                        <Row spacing={1}>
+                            {prefillData?.roles.map(role => <LabelTag label={role} color={colorConfig[role]}/>)}
+                        </Row>
+                    </Field>
+                </Column>
+            </Section>
+        )
+    }
     
     return (
         <Section title={i18n._(t`Accountgegevens`)}>
             <Column spacing={6}>
                 <Field label={i18n._(t`Email`)} horizontal={true}>
-                    <Input name="email" placeholder={i18n._(t`naam@aanbieder.nl`)} />
+                    <Input name="email" placeholder={i18n._(t`naam@aanbieder.nl`)} defaultValue={prefillData?.email} />
                 </Field>
                 <Field label={i18n._(t`Rol`)} horizontal={true}>
                     <Column spacing={3}>
                         <Row>
-                            <RadioButton name={'role'} />
-                            <LabelTag color={LabelColor.red} label={i18n._(t`Coördinator`)} />
+                            <RadioButton name={'role'} value={[Roles.coordinator]} checked={isEqual(prefillData?.roles, [Roles.coordinator])} />
+                            <LabelTag color={colorConfig[Roles.coordinator]} label={i18n._(t`Coördinator`)} />
                         </Row>
                         <Row>
-                            <RadioButton name={'role'} />
-                            <LabelTag color={LabelColor.purple} label={i18n._(t`Begeleider`)} />
+                            <RadioButton name={'role'} value={[Roles.mentor]} checked={isEqual(prefillData?.roles, [Roles.mentor])} />
+                            <LabelTag color={colorConfig[Roles.mentor]} label={i18n._(t`Begeleider`)} />
                         </Row>
                         <Row>
-                            <RadioButton name={'role'} />
+                            <RadioButton name={'role'} value={[Roles.coordinator, Roles.mentor]} checked={isEqual(prefillData?.roles, [Roles.coordinator, Roles.mentor])} />
                             <Row spacing={1}>
-                                <LabelTag color={LabelColor.red} label={i18n._(t`Coördinator`)} />
-                                <LabelTag color={LabelColor.purple} label={i18n._(t`Begeleider`)} />
+                                <LabelTag color={colorConfig[Roles.coordinator]} label={i18n._(t`Coördinator`)} />
+                                <LabelTag color={colorConfig[Roles.mentor]} label={i18n._(t`Begeleider`)} />
                             </Row>
                         </Row>
                         <Row>
-                            <RadioButton name={'role'} />
-                            <LabelTag color={LabelColor.yellow} label={i18n._(t`Vrijwilliger`)} />
+                            <RadioButton name={'role'} value={[Roles.volunteer]} checked={isEqual(prefillData?.roles, [Roles.volunteer])} />
+                            <LabelTag color={colorConfig[Roles.volunteer]} label={i18n._(t`Vrijwilliger`)} />
                         </Row>
                     </Column>
                 </Field>
