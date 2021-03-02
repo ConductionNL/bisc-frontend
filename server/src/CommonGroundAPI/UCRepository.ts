9b7ce8c10ac076092c67b8b9bf5f1052ddfd5bd9
Dirk Kok <dirk@lifely.nl>
2021-03-02 11:15:40 +0100

    feat(TaalhuisEmployee): clean up comments

--- server/src/CommonGroundAPI/UCRepository.ts
+++ server/src/CommonGroundAPI/UCRepository.ts
@@ -21,9 +21,7 @@ export class UCRepository extends BaseRepository {
     }
 
     public makeURLfromID(id: string) {
-        // TODO: Update object URI's to include the component name
         return `https://taalhuizen-bisc.commonground.nu/api/v1/uc${id[0] === '/' ? '' : '/'}${id}`
-        // return `https://taalhuizen-bisc.commonground.nu${id[0] === '/' ? '' : '/'}${id}`
     }
 
     public stripURLfromID(id: string) {
