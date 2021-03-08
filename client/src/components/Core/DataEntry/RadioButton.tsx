c32b4be0e9df48d545ea438c7bd5c4671eb805ab
Rick Woltheus <rick.woltheus@lifely.nl>
2021-03-08 16:27:47 +0100

    fix radiobutton

--- client/src/components/Core/DataEntry/RadioButton.tsx
+++ client/src/components/Core/DataEntry/RadioButton.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import styles from './RadioButton.module.scss'
 
-interface Props {
-    className?: string
-    value?: string | ReadonlyArray<string> | number;
-    checked?: boolean
-    disabled?: boolean
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     name: string
 }
 
-const RadioButton: React.FunctionComponent<Props> = ({ disabled, checked, name, value }) => {
+const RadioButton: React.FunctionComponent<Props> = props => {
     return (
         <div className={styles.container}>
-            <input className={styles.inputField} name={name} type="radio" disabled={disabled} checked={checked} value={value}/>
+            <input type="radio" {...props}/>
             <div className={styles.radio} />
         </div>
     )
