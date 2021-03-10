73664839bb9376cc8a69a538f8aadece40115143
bbawuah <brianbawuah@icloud.com>
2021-03-10 16:30:17 +0100

    fix(input date validator): add input date validator

--- client/src/components/Core/DataEntry/DateInput.tsx
+++ client/src/components/Core/DataEntry/DateInput.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Validator } from '../../../utils/validators/types'
 import Icon from '../Icon/Icon'
 import { IconType } from '../Icon/IconType'
 import styles from './DateInput.module.scss'
@@ -9,9 +10,9 @@ interface Props {
     className?: string
     inputClassName?: string
     value?: string
-    checked?: boolean
     disabled?: boolean
     name?: string
+    validators?: Validator<string | null>[]
     placeholder?: string
     id?: string
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
@@ -19,16 +20,18 @@ interface Props {
 
 const DateInput: React.FunctionComponent<Props> = ({
     disabled,
-    checked,
     name,
     onChange,
     value,
     id,
+    validators,
     placeholder,
     className,
     inputClassName,
 }) => {
     const containerClassNames = classNames(styles.container, className)
+    const [error, setError] = useState<string | null>(null)
+    const date = useRef<HTMLInputElement>(null)
 
     return (
         <div className={containerClassNames}>
@@ -39,12 +42,32 @@ const DateInput: React.FunctionComponent<Props> = ({
                 id={id}
                 placeholder={placeholder}
                 disabled={disabled}
-                checked={checked}
-                onChange={onChange}
+                onBlur={handleOnBlur}
+                onChange={handleOnChange}
                 value={value}
             />
         </div>
     )
+
+    function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+        onChange?.(event)
+    }
+
+    function handleOnBlur(event: React.FocusEvent<HTMLInputElement>) {
+        const value = event.currentTarget.value
+
+        validators?.every(validator => {
+            const result = validator(value)
+            if (result) {
+                setError(result)
+                date.current?.setCustomValidity(result)
+                return false
+            }
+            setError(result)
+            date.current?.setCustomValidity('')
+            return true
+        })
+    }
 }
 
 export default DateInput
