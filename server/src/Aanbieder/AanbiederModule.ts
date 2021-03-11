75f4fdc8860b2e0bad4f116b8a08e644686ba0fc
Joran de Waaij <joran@lifely.nl>
2021-03-11 10:30:58 +0100

    feat(aanbieder): update and delete mutations

--- server/src/Aanbieder/AanbiederModule.ts
+++ server/src/Aanbieder/AanbiederModule.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { CommonGroundAPIModule } from 'src/CommonGroundAPI/CommonGroundAPIModule'
 import { AanbiederResolver } from './AanbiederResolver'
 import { CreateAanbiederService } from './CreateAanbiederService'
+import { DeleteAanbiederService } from './DeleteAanbiederService'
+import { UpdateAanbiederService } from './UpdateAanbiederService'
 
 @Module({
     imports: [CommonGroundAPIModule],
-    providers: [AanbiederResolver, CreateAanbiederService],
+    providers: [AanbiederResolver, CreateAanbiederService, DeleteAanbiederService, UpdateAanbiederService],
 })
 export class AanbiederModule {}
--- server/src/Aanbieder/AanbiederResolver.ts
+++ server/src/Aanbieder/AanbiederResolver.ts
@@ -3,14 +3,20 @@ import { OrganizationRepository, OrganizationTypesEnum } from 'src/CommonGroundA
 import { CurrentUser } from 'src/User/CurrentUserDecorator'
 import { UserEntity } from 'src/User/entities/UserEntity'
 import { CreateAanbiederService } from './CreateAanbiederService'
+import { DeleteAanbiederService } from './DeleteAanbiederService'
 import { AanbiederType } from './types/AanbiederType'
 import { CreateAanbiederInputType } from './types/CreateAanbiederInputType'
+import { DeleteAanbiederInputType } from './types/DeleteAanbiederInputType'
+import { UpdateAanbiederInputType } from './types/UpdateAanbiederInputType'
+import { UpdateAanbiederService } from './UpdateAanbiederService'
 
 @Resolver(() => AanbiederType)
 export class AanbiederResolver {
     public constructor(
         private organizationRepository: OrganizationRepository,
-        private createAanbiederService: CreateAanbiederService
+        private createAanbiederService: CreateAanbiederService,
+        private updateAanbiederService: UpdateAanbiederService,
+        private deleteAanbiederService: DeleteAanbiederService
     ) {}
 
     @Query(() => [AanbiederType])
@@ -28,5 +34,15 @@ export class AanbiederResolver {
     @Mutation(() => AanbiederType)
     public async createAanbieder(@Args() args: CreateAanbiederInputType): Promise<AanbiederType> {
         return this.createAanbiederService.createAanbieder(args)
+    }
+
+    @Mutation(() => AanbiederType)
+    public async updateAanbieder(@Args() args: UpdateAanbiederInputType): Promise<AanbiederType> {
+        return this.updateAanbiederService.updateAanbieder(args)
+    }
+
+    @Mutation(() => Boolean)
+    public async deleteAanbieder(@Args() args: DeleteAanbiederInputType): Promise<boolean> {
+        return !!(await this.deleteAanbiederService.deleteAanbieder(args.id))
     }
 }
