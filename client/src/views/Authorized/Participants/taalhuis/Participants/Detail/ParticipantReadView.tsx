960b8bdb375aad1bdd40aeb7fdac04e3c3b551b7
bbawuah <brianbawuah@icloud.com>
2021-03-17 21:31:10 +0100

    feat(ParticipantReadView): create read view setup

--- client/src/views/Authorized/Participants/taalhuis/Participants/Detail/ParticipantReadView.tsx
+++ client/src/views/Authorized/Participants/taalhuis/Participants/Detail/ParticipantReadView.tsx
@@ -1,13 +1,71 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
 import React from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+import Headline, { SpacingType } from '../../../../../../components/Chrome/Headline'
+import Breadcrumb from '../../../../../../components/Core/Breadcrumb/Breadcrumb'
+import Breadcrumbs from '../../../../../../components/Core/Breadcrumb/Breadcrumbs'
+import ErrorBlock from '../../../../../../components/Core/Feedback/Error/ErrorBlock'
+import Spinner, { Animation } from '../../../../../../components/Core/Feedback/Spinner/Spinner'
+import Center from '../../../../../../components/Core/Layout/Center/Center'
+import Column from '../../../../../../components/Core/Layout/Column/Column'
+import { useMockQuery } from '../../../../../../components/hooks/useMockQuery'
+import { ParticipantDetailParams } from '../../../../../../routes/participants/types'
+import { routes } from '../../../../../../routes/routes'
+import { taalhuisParticipantsCreateResponse } from '../../../mocks/participants'
 
 interface Props {}
 
 interface Props {}
 
 export const ParticipantsReadView: React.FunctionComponent<Props> = () => {
+    const { i18n } = useLingui()
+    const history = useHistory()
+    const params = useParams<ParticipantDetailParams>()
+    const { data, loading, error } = useMockQuery(taalhuisParticipantsCreateResponse)
+
+    if (!params.participantid) {
+        return null
+    }
+
     return (
         <>
-            <h1>Deelnemer 0</h1>
+            <Column spacing={10}>
+                <Headline
+                    title={i18n._(t`Deelnemer ${params.participantname}`)}
+                    spacingType={SpacingType.small}
+                    TopComponent={
+                        <Breadcrumbs>
+                            <Breadcrumb
+                                text={i18n._(t`Deelnemers`)}
+                                to={routes.authorized.participants.taalhuis.participants.overview}
+                            />
+                        </Breadcrumbs>
+                    }
+                />
+            </Column>
         </>
     )
+
+    function renderSection() {
+        if (loading) {
+            return (
+                <Center grow={true}>
+                    <Spinner type={Animation.pageSpinner} />
+                </Center>
+            )
+        }
+
+        if (error) {
+            return (
+                <ErrorBlock
+                    title={i18n._(t`Er ging iets fout`)}
+                    message={i18n._(t`Wij konden de gegevens niet ophalen, probeer het opnieuw`)}
+                />
+            )
+        }
+
+        if (data) {
+        }
+    }
 }
