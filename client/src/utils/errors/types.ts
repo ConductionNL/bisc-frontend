cb1bcff3daa8237dbbab58f5f6a7c0346cf31fff
Rick Woltheus <rick.woltheus@lifely.nl>
2021-03-19 08:58:21 +0100

    add types

---
+++ client/src/utils/errors/types.ts
@@ -0,0 +1,8 @@
+export enum ErrorCode {
+    AuthorizationFailed = 'AUTHORIZATION_VALIDATION',
+    AuthenticationFailed = 'AUTHENTICATION_VALIDATION',
+    InputValidation = 'INPUT_VALIDATION',
+    EntityAlreadyExists = 'ENTITY_ALREADY_EXISTS',
+    PasswordConfirmationFailed = 'PASSWORD_CONFIRMATION_FAILED',
+    GeocodingFailed = 'GEOCODING_FAILED',
+}
