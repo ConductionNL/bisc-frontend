f517aaead2c5b90661a12f7ad2e2e4c81397cb1b
gorkem <gorkem.kockaya@gmail.com>
2021-03-22 20:35:37 +0300

    feat(aanbieder-completed-participants-overview-view): add component

--- client/src/views/Authorized/Supplier/AanbiederView/AanbiederView.tsx
+++ client/src/views/Authorized/Supplier/AanbiederView/AanbiederView.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 import { routes } from 'routes/routes'
 import { AanbiederActiveParticipantsOverviewView } from './AanbiederActiveParticipantsOverviewView'
+import { AanbiederCompletedParticipantsOverviewView } from './AanbiederCompletedParticipantsOverviewView'
+import { AanbiederReferredParticipantsOverviewView } from './AanbiederReferredParticipantsOverviewView'
 import { AanbiederParticipantView } from './AanbiederParticipantView/AanbiederParticipantView'
 
 export const AanbiederView: React.FunctionComponent = () => {
@@ -11,6 +13,8 @@ export const AanbiederView: React.FunctionComponent = () => {
         <Switch>
             <Redirect path={supplierRoute.index} exact={true} to={supplierRoute.participants.active} />
             <Route path={supplierRoute.participants.active} component={AanbiederActiveParticipantsOverviewView} />
+            <Route path={supplierRoute.participants.completed} component={AanbiederCompletedParticipantsOverviewView} />
+            <Route path={supplierRoute.participants.referred} component={AanbiederReferredParticipantsOverviewView} />
             <Route path={supplierRoute.participant.overview} component={AanbiederParticipantView} />
         </Switch>
     )
