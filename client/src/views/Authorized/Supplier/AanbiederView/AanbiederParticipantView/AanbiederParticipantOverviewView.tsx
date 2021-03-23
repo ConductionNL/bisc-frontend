03c73c7ccde6f1c97326453bc9246f43e1f54ebf
gorkem <gorkem.kockaya@gmail.com>
2021-03-23 18:09:46 +0300

    wip(aanbieder-participant-overview-view): render data

--- client/src/views/Authorized/Supplier/AanbiederView/AanbiederParticipantView/AanbiederParticipantOverviewView.tsx
+++ client/src/views/Authorized/Supplier/AanbiederView/AanbiederParticipantView/AanbiederParticipantOverviewView.tsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+
+import Headline, { SpacingType } from 'components/Chrome/Headline'
+import ErrorBlock from 'components/Core/Feedback/Error/ErrorBlock'
+import Spinner, { Animation } from 'components/Core/Feedback/Spinner/Spinner'
+import Center from 'components/Core/Layout/Center/Center'
+import Column from 'components/Core/Layout/Column/Column'
+import { AanbiederParticipantTab, AanbiederParticipantTabs } from 'components/Domain/Aanbieder/AanbiederParticipantTab'
+import { useMockQuery } from 'components/hooks/useMockQuery'
+import { aanbiederParticipantDetail, AanbiederParticipantDetail } from '../mocks'
 
 interface Props {
     participantId: number
@@ -6,9 +17,38 @@ interface Props {
 
 // TODO
 export const AanbiederParticipantOverviewView: React.FunctionComponent<Props> = ({ participantId }) => {
+    const { i18n } = useLingui()
+    const { data, loading, error } = useMockQuery<AanbiederParticipantDetail>(aanbiederParticipantDetail)
+
     return (
-        <div>
-            <p>id: {participantId}</p>
-        </div>
+        <>
+            <Headline spacingType={SpacingType.small} title={i18n._(t`Deelnemers`)} />
+            <Column spacing={10}>
+                <AanbiederParticipantTabs currentTab={AanbiederParticipantTab.overview} />
+                {renderList()}
+            </Column>
+        </>
     )
+
+    function renderList() {
+        if (loading) {
+            return (
+                <Center grow={true}>
+                    <Spinner type={Animation.pageSpinner} />
+                </Center>
+            )
+        }
+
+        if (error) {
+            return (
+                <ErrorBlock
+                    title={i18n._(t`Er ging iets fout`)}
+                    message={i18n._(t`Wij konden de gegevens niet ophalen, probeer het opnieuw`)}
+                />
+            )
+        }
+
+        // TODO: do something with the data here
+        return null
+    }
 }
--- client/src/views/Authorized/Supplier/AanbiederView/mocks.ts
+++ client/src/views/Authorized/Supplier/AanbiederView/mocks.ts
@@ -11,6 +11,44 @@ export interface AanbiederParticipant {
     referredAt?: Date
 }
 
+export interface AanbiederParticipantDetail extends AanbiederParticipant {
+    fullName: string
+    gender: Gender
+    birthDate: Date
+    address: Address
+    customer: Customer
+    isCivicIntegrationRequired: boolean
+    civicIntegrationReason: string
+}
+
+enum Gender {
+    man = 'Man',
+    woman = 'Woman',
+}
+
+interface Address {
+    id: number
+    street: string
+    building: number
+    apartment: string
+    postcode: string
+    city: string
+    phone: string
+    contactPreference: ContactPreference
+}
+
+enum ContactPreference {
+    call = 'call',
+    text = 'text',
+    email = 'email',
+}
+
+interface Customer {
+    id: number
+    fullName: string
+    assignedAt: Date
+}
+
 export const aanbiederParticipantsMock: AanbiederParticipant[] = times(16, i => ({
     id: i,
     lastName: 'somelastname',
@@ -19,3 +57,30 @@ export const aanbiederParticipantsMock: AanbiederParticipant[] = times(16, i => 
     referredBy: !!(i & 1) ? 'somereferrer' : undefined,
     referredAt: !!(i & 1) ? new Date() : undefined,
 }))
+
+export const aanbiederParticipantDetail: AanbiederParticipantDetail = {
+    id: 1,
+    lastName: 'somelastname',
+    firstName: 'somefirstname',
+    fullName: 'somefirstname somelastname',
+    gender: Gender.man,
+    birthDate: new Date(),
+    isReferred: false,
+    isCivicIntegrationRequired: false,
+    civicIntegrationReason: 'Afgerond',
+    address: {
+        id: 1,
+        street: 'somestreetname',
+        building: 1,
+        apartment: 'A',
+        postcode: '1234 AB',
+        city: 'somecity',
+        phone: '123456789',
+        contactPreference: ContactPreference.call,
+    },
+    customer: {
+        id: 1,
+        fullName: 'somecustomer fullname',
+        assignedAt: new Date(),
+    },
+}
