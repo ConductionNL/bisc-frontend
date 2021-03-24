f9fdc1aedd501bc5d8c138e4faf70e9974a2e1bb
gorkem <gorkem.kockaya@gmail.com>
2021-03-24 13:21:54 +0300

    feat(aanbieder-participant-registration-view): add component

--- client/src/views/Authorized/Supplier/AanbiederView/AanbiederParticipantView/AanbiederParticipantView.tsx
+++ client/src/views/Authorized/Supplier/AanbiederView/AanbiederParticipantView/AanbiederParticipantView.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch, useLocation } from 'react-router'
 import { routes } from 'routes/routes'
 import { AanbiederParticipantOverviewView } from './AanbiederParticipantOverviewView'
+import { AanbiederParticipantRegistrationView } from './AanbiederParticipantRegistrationView'
 
 interface LocationStateProps {
     participantId: number
@@ -16,6 +17,7 @@ export const AanbiederParticipantView: React.FunctionComponent = () => {
     return (
         <Switch>
             <Route path={basePath.overview} render={() => <AanbiederParticipantOverviewView {...props} />} />
+            <Route path={basePath.registration} render={() => <AanbiederParticipantRegistrationView {...props} />} />
         </Switch>
     )
 }
