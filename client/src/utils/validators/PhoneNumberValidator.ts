a2787d3d9001b408cc3ef4f3c9b6b2e725656538
gorkem <gorkem.kockaya@gmail.com>
2021-03-25 09:47:20 +0300

    feat(phone-number-validator): use validator package

--- client/src/utils/validators/PhoneNumberValidator.ts
+++ client/src/utils/validators/PhoneNumberValidator.ts
@@ -1,11 +1,10 @@
 import { i18n } from '@lingui/core'
 import { t } from '@lingui/macro'
+import isMobilePhone from 'validator/lib/isMobilePhone'
 
 class PhoneNumber {
     public isPhoneNumber = (value: string | null) => {
-        const phoneNumberRegex = /^((\+|00(\s|\s?-\s?)?)31(\s|\s?-\s?)?(\(0\)[-\s]?)?|0)[1-9]((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]$/
-        const test = phoneNumberRegex.test(String(value))
-        if (!test) {
+        if (!value || !isMobilePhone(value, 'nl-NL')) {
             return i18n._(t`Dit is geen telefoon nummer`)
         }
         return null
