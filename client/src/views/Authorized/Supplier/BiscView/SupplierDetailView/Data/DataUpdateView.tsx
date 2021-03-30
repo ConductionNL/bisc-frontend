05169864c5bc6fb936c78bcd216aea5b3b56ad8f
Rick Woltheus <rick.woltheus@lifely.nl>
2021-03-30 11:29:29 +0200

    update supplier mutation

--- client/src/views/Authorized/Supplier/BiscView/SupplierDetailView/Data/DataUpdateView.tsx
+++ client/src/views/Authorized/Supplier/BiscView/SupplierDetailView/Data/DataUpdateView.tsx
@@ -46,21 +46,19 @@ const DataUpdateView: React.FunctionComponent<Props> = props => {
     const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const formData = Forms.getFormDataFromFormEvent<FormModel>(e)
-
-        // TODO: when formData is not available, send back data from useAanbiederQuery
         const response = await updateSupplier({
             variables: {
                 id: routeState.supplierId,
                 address: {
-                    street: formData.street || '',
-                    houseNumber: formData.streetNr || '',
+                    street: formData.street,
+                    houseNumber: formData.streetNr,
                     houseNumberSuffix: formData.addition,
-                    postalCode: formData.postcode || '',
-                    locality: formData.city || '',
+                    postalCode: formData.postcode,
+                    locality: formData.city,
                 },
-                name: formData.branch || '',
-                email: formData.email || '',
-                phoneNumber: formData.phone || '',
+                name: formData.branch,
+                email: formData.email,
+                phoneNumber: formData.phone,
             },
         })
 
