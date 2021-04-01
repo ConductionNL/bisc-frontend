305da79efc582f1f44ad9028bb1e651dea264afb
bbawuah <brianbawuah@icloud.com>
2021-04-01 10:14:25 +0200

    fix(pr fixes): add pr fixes

--- client/src/components/Core/Actionbar/Actionbar.tsx
+++ client/src/components/Core/Actionbar/Actionbar.tsx
@@ -9,16 +9,18 @@ interface Props {
     RightComponent: JSX.Element
 }
 
-// TODO: Th
 const Actionbar: React.FunctionComponent<Props> = props => {
     const { className, LeftComponent, RightComponent } = props
     const containerClassName = classNames(styles.container, className)
 
     return (
-        <div className={containerClassName}>
-            {LeftComponent && LeftComponent}
-            {RightComponent && <div className={styles.rightContainer}>{RightComponent}</div>}
-        </div>
+        <>
+            {/*TODO: The fragment is a quickfix. We should create a better solution for this*/}
+            <div style={{ width: 100 }} className={containerClassName}>
+                {LeftComponent && LeftComponent}
+                {RightComponent && <div className={styles.rightContainer}>{RightComponent}</div>}
+            </div>
+        </>
     )
 }
 
