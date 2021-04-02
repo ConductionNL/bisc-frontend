1e6ad500b1f1fbaee5e69997024ad2e2a1f777e0
gorkem <gorkem.kockaya@gmail.com>
2021-04-02 11:23:39 +0300

    feat(breadcrumbs): update component to take breadcrumb item

--- client/src/components/Core/Breadcrumb/Breadcrumbs.tsx
+++ client/src/components/Core/Breadcrumb/Breadcrumbs.tsx
@@ -2,16 +2,30 @@ import React from 'react'
 import classNames from 'classnames'
 
 import styles from './Breadcrumbs.module.scss'
+import Breadcrumb from './Breadcrumb'
+
+interface BreadCrumbItem {
+    label: string
+    to: string
+}
 
 interface Props {
     className?: string
+    breadcrumbItems: BreadCrumbItem[]
+    children: undefined // this is to enforce the component to be self-closing
 }
 
 const Breadcrumbs: React.FunctionComponent<Props> = props => {
-    const { className, children } = props
+    const { className, breadcrumbItems } = props
     const containerClassNames = classNames(styles.container, className)
 
-    return <div className={containerClassNames}>{children}</div>
+    return (
+        <div className={containerClassNames}>
+            {breadcrumbItems.map((item, i) => (
+                <Breadcrumb key={`breadcrumb-${i}`} text={item.label} to={item.to} />
+            ))}
+        </div>
+    )
 }
 
 export default Breadcrumbs
