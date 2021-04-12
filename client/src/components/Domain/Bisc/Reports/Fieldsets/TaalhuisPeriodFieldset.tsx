30b3d88ac7157ad475d342641f3b610f89c4f799
Rick Woltheus <rick.woltheus@lifely.nl>
2021-04-12 11:22:21 +0200

    fix styles

--- client/src/components/Domain/Bisc/Reports/Fieldsets/TaalhuisPeriodFieldset.tsx
+++ client/src/components/Domain/Bisc/Reports/Fieldsets/TaalhuisPeriodFieldset.tsx
@@ -1,5 +1,6 @@
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
+import classNames from 'classnames'
 import DateInput from 'components/Core/DataEntry/DateInput'
 import Select from 'components/Core/DataEntry/Select'
 import Field from 'components/Core/Field/Field'
@@ -7,6 +8,7 @@ import Column from 'components/Core/Layout/Column/Column'
 import Row from 'components/Core/Layout/Row/Row'
 import { TaalhuizenQuery } from 'generated/graphql'
 import React from 'react'
+import styles from './TaalhuisPeriodFieldset.module.scss'
 
 interface Props {
     prefillData?: TaalhuisPeriodFieldsetPrefillData
@@ -29,12 +31,16 @@ export const TaalhuisPeriodFieldset: React.FunctionComponent<Props> = props => {
     const { queryData, hideTaalhuisSelect } = props
     const { i18n } = useLingui()
 
+    const PeriodFieldClassNames = classNames({
+        [styles.periodFieldWithoutSelect]: hideTaalhuisSelect,
+        [styles.periodFieldWithSelect]: !hideTaalhuisSelect,
+    })
     return (
         <Column spacing={4}>
             <Row spacing={5}>
                 {/* inline styles were a quickfix */}
-                {hideTaalhuisSelect && (
-                    <div style={{ width: '50%' }}>
+                {!hideTaalhuisSelect && (
+                    <div className={styles.taalhuisSelectWrapper}>
                         <Field label={i18n._(t`Taalhuis`)} grow={true}>
                             <Select
                                 list="taalhuis"
@@ -46,13 +52,13 @@ export const TaalhuisPeriodFieldset: React.FunctionComponent<Props> = props => {
                         </Field>
                     </div>
                 )}
-                <Row spacing={5}>
-                    <div style={{ width: 128 }}>
+                <Row spacing={5} className={styles.periodWrapper}>
+                    <div className={PeriodFieldClassNames}>
                         <Field label={i18n._(t`Periode van`)} grow={true}>
                             <DateInput name={'periodFrom'} placeholder={i18n._(t`DD/MM/YYYY`)} grow={true} />
                         </Field>
                     </div>
-                    <div style={{ width: 128 }}>
+                    <div className={PeriodFieldClassNames}>
                         <Field label={i18n._(t`periode tot`)} grow={true}>
                             <DateInput name={'periodTo'} placeholder={i18n._(t`DD/MM/YYYY`)} grow={true} />
                         </Field>
