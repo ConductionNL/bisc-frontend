54e856630559b9c48904e583d916039f5fe7d54a
bbawuah <brianbawuah@icloud.com>
2021-04-14 10:32:17 +0200

    fix(breadcrumbs): add breadcrumb fix

--- client/src/components/Domain/Bisc/Taalhuizen/Breadcrumbs/TaalhuizenParticipantsLearningNeedsBreadCrumbs.tsx
+++ client/src/components/Domain/Bisc/Taalhuizen/Breadcrumbs/TaalhuizenParticipantsLearningNeedsBreadCrumbs.tsx
@@ -1,7 +1,6 @@
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import Breadcrumb from 'components/Core/Breadcrumb/Breadcrumb'
-import Breadcrumbs from 'components/Core/Breadcrumb/Breadcrumbs'
+import { Breadcrumbs } from 'components/Core/Breadcrumbs/Breadcrumbs'
 import React from 'react'
 import { routes } from 'routes/routes'
 import { ParticipantDetailLocationStateProps } from 'views/Authorized/Participants/taalhuis/Participants/Detail/ParticipantsDetailView'
@@ -14,34 +13,36 @@ export const TaalhuizenParticipantsLearningNeedsBreadCrumbs: React.FC<Props> = (
     const { i18n } = useLingui()
 
     return (
-        <Breadcrumbs>
-            <Breadcrumb
-                text={i18n._(t`Deelnemers`)}
-                to={{
-                    pathname: routes.authorized.participants.taalhuis.participants.overview,
-                    hash: '',
-                    search: '',
-                    state: routeState,
-                }}
-            />
-            <Breadcrumb
-                text={i18n._(t`Leervragen`)}
-                to={{
-                    pathname: routes.authorized.participants.taalhuis.participants.detail.goals.overview,
-                    hash: '',
-                    search: '',
-                    state: routeState,
-                }}
-            />
-            <Breadcrumb
-                text={i18n._(t`Met computers leren werken`)}
-                to={{
-                    pathname: routes.authorized.participants.taalhuis.participants.detail.goals.detail.read,
-                    hash: '',
-                    search: '',
-                    state: routeState,
-                }}
-            />
-        </Breadcrumbs>
+        <Breadcrumbs
+            breadcrumbItems={[
+                {
+                    label: i18n._(t`Deelnemers`),
+                    to: {
+                        pathname: routes.authorized.participants.taalhuis.participants.overview,
+                        hash: '',
+                        search: '',
+                        state: routeState,
+                    },
+                },
+                {
+                    label: i18n._(t`Leervragen`),
+                    to: {
+                        pathname: routes.authorized.participants.taalhuis.participants.detail.goals.overview,
+                        hash: '',
+                        search: '',
+                        state: routeState,
+                    },
+                },
+                {
+                    label: i18n._(t`Met computers leren werken`),
+                    to: {
+                        pathname: routes.authorized.participants.taalhuis.participants.detail.goals.detail.read,
+                        hash: '',
+                        search: '',
+                        state: routeState,
+                    },
+                },
+            ]}
+        />
     )
 }
