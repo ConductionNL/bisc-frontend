1e8f22da2ea578681939b452914896cf5a0cca45
bbawuah <brianbawuah@icloud.com>
2021-04-23 14:11:10 +0200

    feat(taalhuis fieldset): create taalhuisFieldset

---
+++ client/src/components/Domain/PublicRegistration/Fields/Fieldsets/LanguageHouseFieldset.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import Select from 'components/Core/DataEntry/Select'
+import Field from 'components/Core/Field/Field'
+import Section from 'components/Core/Field/Section'
+import Column from 'components/Core/Layout/Column/Column'
+import { LanguageHousesQuery } from 'generated/graphql'
+
+interface Props {
+    queryData?: LanguageHousesQuery
+}
+
+export interface LanguageHouseFieldsetModel {
+    registeringParty: string | null
+    registratorLastName: string | null
+    registratorAddition?: string | null
+    registratorNickName: string | null
+    registratorEmail: string | null
+    registratorPhone: string | null
+}
+
+const LanguageHouseFieldset: React.FunctionComponent<Props> = props => {
+    const { queryData } = props
+    const { i18n } = useLingui()
+
+    return (
+        <Section title={i18n._(t`Taalhuis`)}>
+            <Column spacing={4}>
+                <Field label={i18n._(t`Taalhuis`)} horizontal={true} required={true}>
+                    <Select
+                        list="taalhuis"
+                        name={'taalhuis'}
+                        placeholder={i18n._(t`Selecteer Taalhuis...`)}
+                        options={getTaalhuisOptions()}
+                    />
+                </Field>
+            </Column>
+        </Section>
+    )
+
+    function getTaalhuisOptions() {
+        return queryData ? queryData.languageHouses.map(item => ({ label: item.name, value: item.id })) : []
+    }
+}
+
+export default LanguageHouseFieldset
--- client/src/components/Domain/PublicRegistration/Fields/PublicRegistrationFields.tsx
+++ client/src/components/Domain/PublicRegistration/Fields/PublicRegistrationFields.tsx
@@ -3,6 +3,7 @@ import SectionTitle from 'components/Core/Text/SectionTitle'
 import styles from './PublicRegistrationFields.module.scss'
 import RegistratorInformationFieldset from './Fieldsets/RegistratorInformationFieldset'
 import HorizontalRule from 'components/Core/HorizontalRule/HorizontalRule'
+import LanguageHouseFieldset from './Fieldsets/LanguageHouseFieldset'
 
 interface Props {}
 
@@ -11,6 +12,9 @@ export const PublicRegistrationFields: React.FC<Props> = () => {
         <div className={styles.container}>
             <SectionTitle heading={'H3'} title={'Aanmelder'} className={styles.sectionTitle} />
             <RegistratorInformationFieldset />
+            <HorizontalRule />
+            <SectionTitle heading={'H3'} title={'Taalhuis'} className={styles.sectionTitle} />
+            <LanguageHouseFieldset />
             <HorizontalRule />
         </div>
     )
