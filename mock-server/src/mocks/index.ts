2c1653018bc642aa45ce05377be17d9fa99ff313
Jesse de Vries <>
2021-04-26 13:55:51 +0200

    feat(mocks): add mocks for address types

--- mock-server/src/mocks/index.ts
+++ mock-server/src/mocks/index.ts
@@ -1,4 +1,4 @@
-import { UserEnvironmentEnum, UserRoleEnum } from '../../../client/src/generated/graphql'
+import { StudentContactPreferenceEnum, UserEnvironmentEnum, UserRoleEnum } from '../../../client/src/generated/graphql'
 import { base64ExamplePdf } from './base64examplePDF'
 
 export const mocks = {
@@ -9,16 +9,35 @@ export const mocks = {
             givenName: 'Jesse',
             additionalName: 'Boyd',
             familyName: 'de Vries',
-            userEnvironment: UserEnvironmentEnum.Aanbieder,
+            userEnvironment: UserEnvironmentEnum.Taalhuis,
             organizationId: '123-organization',
             organizationName: 'organizationName',
             dateCreated: new Date(),
             dateModified: new Date(),
-            userRoles: [{id: 1, name: UserRoleEnum.AanbiederCoordinator }],
+            userRoles: [{id: 1, name: UserRoleEnum.TaalhuisCoordinator }],
         })
     }),
     Mutation: () => ({
         downloadProviderEmployeeDocument: () => ({ data: { base64data: base64ExamplePdf } }),
         downloadStudentDocument: () => ({ data: { base64data: base64ExamplePdf } }),
     }),
+    ProviderAddressType: () => addressFields,
+    ProviderEmployeeAddressType: () => addressFields,
+    LanguageHouseAddressType: () => addressFields,
+    StudentContactType: () => ({
+        ...addressFields,
+        email: 'john@example.org',
+        telephone: '0612345678',
+        contactPersonTelephone: '0612345678',
+        contactPreference: StudentContactPreferenceEnum.Email,
+        contactPreferenceOther: 'other',
+    }),
+}
+
+const addressFields = {
+    street: 'Prinsengracht',
+    houseNumber: 197,
+    houseNumberSuffix: 'D',
+    postalCode: '1015DT',
+    locality: 'Amsterdam'
 }
