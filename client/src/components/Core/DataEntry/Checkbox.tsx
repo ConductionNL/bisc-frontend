13ebe7cdceb0b0c1a46224b6525fe97827ac12b5
Rick Woltheus <rick.woltheus@lifely.nl>
2021-04-28 09:44:35 +0200

    make checkbox controlled

--- client/src/components/Core/DataEntry/Checkbox.tsx
+++ client/src/components/Core/DataEntry/Checkbox.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import Icon from '../Icon/Icon'
 import { IconType } from '../Icon/IconType'
 import styles from './Checkbox.module.scss'
@@ -15,18 +15,36 @@ interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'
 }
 
 const Checkbox: React.FunctionComponent<Props> = props => {
-    const { className, color = CheckboxColor.orange, inputClassName, ...restProps } = props
+    const { className, color = CheckboxColor.orange, inputClassName, defaultChecked, onChange, ...restProps } = props
     const containerClassNames = classNames(styles.container, className, {
         [styles.orange]: color === CheckboxColor.orange,
         [styles.green]: color === CheckboxColor.green,
     })
+    const [checked, setChecked] = useState(false)
+
+    useEffect(() => {
+        setChecked(!!defaultChecked)
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [defaultChecked])
 
     return (
         <div className={containerClassNames}>
-            <input {...restProps} className={classNames(styles.inputField, inputClassName)} type="checkbox" />
+            <input
+                {...restProps}
+                checked={checked}
+                onChange={handleOnChange}
+                className={classNames(styles.inputField, inputClassName)}
+                type="checkbox"
+            />
             <Icon className={styles.checkmark} type={IconType.checkmark} />
         </div>
     )
+
+    function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
+        setChecked(e.currentTarget.checked)
+        onChange?.(e)
+    }
 }
 
 export default Checkbox
