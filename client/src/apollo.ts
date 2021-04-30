a4dee7439823a7e60f66609e398ef757502f9b0e
Jesse de Vries <>
2021-04-30 10:22:14 +0200

    fix(apollo): use url from env var

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -6,7 +6,7 @@ import { accessTokenLocalstorageKey } from './components/Providers/SessionProvid
 import { ErrorLinkHandler } from './utils/errors/ErrorLinkHandler'
 
 const httpLink = createHttpLink({
-    uri: 'https://taalhuizen-bisc.commonground.nu/api/v1/taal/graphql',
+    uri: window.ENVIRONMENT.GRAPHQL_URI,
 })
 
 const authLink = setContext((_, { headers }) => {
