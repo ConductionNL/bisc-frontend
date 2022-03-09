b2853faac522063d746a1f0c3ceda102e7b711a6
Jesse de Vries <jessedvrs@gmail.com>
2022-03-09 16:19:23 +0100

    perf(organizations): fix double TH detail call

--- client/src/views/Authorized/Bisc/Taalhuizen/TaalhuizenDetail/Coworkers/CoworkersView.tsx
+++ client/src/views/Authorized/Bisc/Taalhuizen/TaalhuizenDetail/Coworkers/CoworkersView.tsx
@@ -1,4 +1,9 @@
-import { Organization } from 'api/types/types'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { useGetTaalhuisOrganization } from 'api/organization/organization'
+import ErrorBlock from 'components/Core/Feedback/Error/ErrorBlock'
+import Spinner, { Animation } from 'components/Core/Feedback/Spinner/Spinner'
+import Center from 'components/Core/Layout/Center/Center'
 import React from 'react'
 import { Route, RouteComponentProps, Switch } from 'react-router-dom'
 import { BiscTaalhuizenDetailRouteParams } from 'routes/bisc/biscRoutes'
@@ -8,29 +13,47 @@ import CoworkersCreateView from './CoworkersCreateView'
 import CoworkersOverviewView from './CoworkersOverviewView'
 import { CoworkersDetailView } from './detail/CoworkersDetailView'
 
-interface Props extends RouteComponentProps<BiscTaalhuizenDetailRouteParams> {
-    organization: Organization
-}
+interface Props extends RouteComponentProps<BiscTaalhuizenDetailRouteParams> {}
 
 export const CoworkersView: React.FunctionComponent<Props> = props => {
-    const { organization } = props
+    const { languageHouseId } = props.match.params
+    const { i18n } = useLingui()
+
+    const { data, loading, error } = useGetTaalhuisOrganization(languageHouseId)
+
+    if (loading) {
+        return (
+            <Center grow={true}>
+                <Spinner type={Animation.pageSpinner} />
+            </Center>
+        )
+    }
+
+    if (error || !data) {
+        return (
+            <ErrorBlock
+                title={i18n._(t`Er ging iets fout`)}
+                message={i18n._(t`Wij konden de gegevens niet ophalen, probeer het opnieuw`)}
+            />
+        )
+    }
 
     return (
         <Switch>
             <Route
                 path={routes.authorized.bisc.taalhuizen.detail().coworkers.index}
                 exact={true}
-                render={props => <CoworkersOverviewView organization={organization} {...props} />}
+                render={props => <CoworkersOverviewView organization={data} {...props} />}
             />
             <Route
                 path={routes.authorized.bisc.taalhuizen.detail().coworkers.create}
                 exact={true}
-                render={props => <CoworkersCreateView languageHouse={organization} {...props} />}
+                render={props => <CoworkersCreateView languageHouse={data} {...props} />}
             />
 
             <Route
                 path={routes.authorized.bisc.taalhuizen.detail().coworkers.detail().index}
-                render={props => <CoworkersDetailView organization={organization} {...props} />}
+                render={props => <CoworkersDetailView organization={data} {...props} />}
             />
 
             <Route component={NotFoundView} />
--- client/src/views/Authorized/Bisc/Taalhuizen/TaalhuizenDetail/TaalhuizenDetailView.tsx
+++ client/src/views/Authorized/Bisc/Taalhuizen/TaalhuizenDetail/TaalhuizenDetailView.tsx
@@ -1,43 +1,15 @@
 import React from 'react'
-import { t } from '@lingui/macro'
-import { useLingui } from '@lingui/react'
 import { Switch, Redirect, Route, RouteComponentProps } from 'react-router-dom'
 import { NotFoundView } from '../../../../Generic/NotFoundView'
-import Spinner, { Animation } from 'components/Core/Feedback/Spinner/Spinner'
 import { TaalhuisDetailDataView } from './Data/TaalhuisDetailDataView'
 import { routes } from 'routes/routes'
 import { TaalhuisDetailUpdateView } from './Data/TaalhuisDetailUpdateView'
 import { CoworkersView } from './Coworkers/CoworkersView'
 import { BiscTaalhuizenDetailRouteParams } from 'routes/bisc/biscRoutes'
-import Center from 'components/Core/Layout/Center/Center'
-import ErrorBlock from 'components/Core/Feedback/Error/ErrorBlock'
-import { useGetTaalhuisOrganization } from 'api/organization/organization'
 
 interface Props extends RouteComponentProps<BiscTaalhuizenDetailRouteParams> {}
 
 const TaalhuizenDetailView: React.FunctionComponent<Props> = props => {
-    const { languageHouseId } = props.match.params
-    const { i18n } = useLingui()
-
-    const { data, loading, error } = useGetTaalhuisOrganization(languageHouseId)
-
-    if (loading) {
-        return (
-            <Center grow={true}>
-                <Spinner type={Animation.pageSpinner} />
-            </Center>
-        )
-    }
-
-    if (error || !data) {
-        return (
-            <ErrorBlock
-                title={i18n._(t`Er ging iets fout`)}
-                message={i18n._(t`Wij konden de gegevens niet ophalen, probeer het opnieuw`)}
-            />
-        )
-    }
-
     return (
         <Switch>
             <Redirect
@@ -58,7 +30,7 @@ const TaalhuizenDetailView: React.FunctionComponent<Props> = props => {
 
             <Route
                 path={routes.authorized.bisc.taalhuizen.detail().coworkers.index}
-                render={props => <CoworkersView organization={data} {...props} />}
+                render={props => <CoworkersView {...props} />}
             />
             <Route component={NotFoundView} />
         </Switch>
